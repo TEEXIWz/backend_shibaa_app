890e0741fc32522086aeae7aa046e171381ddbea
“Milolikaxx” <nusitday106773@gmail.com>
2023-10-29 00:45:18 +0700

    user login register editUser

--- api/user.php
+++ api/user.php
@@ -5,30 +5,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 //register
 $app->post('/user/register', function (Request $request, Response $response, $args) {
-    $conn=$GLOBALS['conn'];
+    $conn = $GLOBALS['conn'];
 
     $body = $request->getBody();
     $jsonData = json_decode($body, true);
-    
-    $hashpwd=password_hash($jsonData['password'],PASSWORD_DEFAULT);
 
-    $sql="SELECT * FROM user WHERE username = ?";
-    $stmt=$conn->prepare($sql);
-    $stmt->bind_param("s",$jsonData['username']);
+    $hashpwd = password_hash($jsonData['password'], PASSWORD_DEFAULT);
+
+    $sql = "SELECT * FROM user WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $jsonData['username']);
     $stmt->execute();
 
-    $result=$stmt->get_result();
+    $result = $stmt->get_result();
 
-    if ($result->num_rows==0) {
-        $sql="INSERT INTO user (username,password,name,phone,status) VALUEs (?, ?, ?, ?, 2)";
-        $stmt=$conn->prepare($sql);
-        $stmt->bind_param("ssss",$jsonData['username'],$hashpwd,$jsonData['name'],$jsonData['phone']);
+    if ($result->num_rows == 0) {
+        $sql = "INSERT INTO user (username,password,name,img) VALUES (?, ?, ?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssss", $jsonData['username'], $hashpwd, $jsonData['name'], $jsonData['img']);
         $stmt->execute();
 
         $response->getBody()->write(json_encode("สมัครสมาชิกสำเร็จ", JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
         return $response->withStatus(201);
-    }
-    else{
+    } else {
         $response->getBody()->write(json_encode("มีชื่อผู้นี้อยู่แล้ว", JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
         return $response->withStatus(202);
     }
@@ -36,41 +35,60 @@ $app->post('/user/register', function (Request $request, Response $response, $ar
 
 //login
 $app->post('/user/login', function (Request $request, Response $response) {
-    $conn=$GLOBALS['conn'];
+    $conn = $GLOBALS['conn'];
 
     $body = $request->getBody();
     $jsonData = json_decode($body, true);
-    $username=$jsonData['username'];
-    $password=$jsonData['password'];
+    $username = $jsonData['username'];
+    $password = $jsonData['password'];
 
-    $sql="  SELECT  * 
+    $sql = "  SELECT  * 
             FROM    user 
-            WHERE   username = ? 
-            AND     status = ?";
-    $stmt=$conn->prepare($sql);
-    $stmt->bind_param("si",$username,$jsonData['status']);
+            WHERE   username = ? ";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
     $stmt->execute();
 
-    $result=$stmt->get_result();
-    $status=201;
-    if ($result->num_rows>0) {
-        $row=$result->fetch_assoc();
-        $pwdIndb=$row['password'];
+    $result = $stmt->get_result();
+    $status = 201;
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $pwdIndb = $row['password'];
 
-        if (password_verify($password,$pwdIndb)) {
+        if (password_verify($password, $pwdIndb)) {
             $response->getBody()->write(json_encode($row, JSON_UNESCAPED_UNICODE));
-            $status=200;
-        }
-        else{
+            $status = 200;
+        } else {
             $response->getBody()->write(json_encode("รหัสผ่านผิด", JSON_UNESCAPED_UNICODE));
         }
-    }
-    else {
+    } else {
         $response->getBody()->write(json_encode("ชื่อผู้ใช้ผิด", JSON_UNESCAPED_UNICODE));
     }
     return $response->withStatus($status);
 });
 
+//editUser
+$app->put('/user/edit/{id}', function (Request $request, Response $response, $args) {
+    $conn = $GLOBALS['conn'];
+    $json = $request->getBody();
+    $jsonData = json_decode($json, true);
+    $id = $args['id'];
+    $hashpwd = password_hash($jsonData['password'], PASSWORD_DEFAULT);
+    $sql = 'update user set name=?, username=? , password=? , img=? where uid = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ssssi', $jsonData['name'], $jsonData['username'], $hashpwd, $jsonData['img'], $id);
+    $stmt->execute();
+    $affected = $stmt->affected_rows;
+    if ($affected > 0) {
+        $data = ["affected_rows" => $affected];
+        $response->getBody()->write(json_encode($data));
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+});
+
+//allUser
 $app->get('/user', function (Request $request, Response $response) {
     $conn = $GLOBALS['conn'];
     $sql = 'SELECT *
